1075a1fa39e18fbdc8f82bd902d2520ad7b1cfb3
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-05 20:28:49 +0500

    Task0

---
+++ Tyuiu.BatTI.Sprint2.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BatTI.Sprint2.Task0.V27.Lib;
+
+namespace Tyuiu.BatTI.Sprint2.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGerCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 1305;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, false, true, false, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint2.Task0.V27/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.BatTI.Sprint2.Task0.V27.Lib;
+
+DataService ds = new DataService();
+
+int x = 1305;
+int y = 275;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.Title = "Спринт #2 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #2                                                                *");
+Console.WriteLine("* Тема: Операции сравнения                                                 *");
+Console.WriteLine("* Задание #0                                                               *");
+Console.WriteLine("* Вариант #27                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,     *");
+Console.WriteLine("* вернет логическую последовательность True False True Fakse Fakse True    *");
+Console.WriteLine("*                                                                          *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
