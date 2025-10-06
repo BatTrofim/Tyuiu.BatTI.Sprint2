9d17aedfbba149a3cfaf9c0d02b3388f62150fdb
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-06 15:55:36 +0500

    Task1

--- Tyuiu.BatTI.Sprint2.Task1.V3/Program.cs
+++ Tyuiu.BatTI.Sprint2.Task1.V3/Program.cs
@@ -1,30 +1,34 @@
-﻿using Tyuiu.BatTI.Sprint2.Task0.V27.Lib;
+﻿using Tyuiu.BatTI.Sprint2.Task1.V3.Lib;
 
 DataService ds = new DataService();
 
-int x = 1305;
-int y = 275;
+int a = 185;
+int b = 316;
+int c = 134;
+int d = 134;
 bool[] res = new bool[6];
-res = ds.GetCompareOperations(x, y);
+res = ds.GetLogicOperations(a, b, c, d);
 
 Console.Title = "Спринт #2 | Выполнил: Бат Т.И. | ПИНб-25-1";
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* Спринт #2                                                                *");
-Console.WriteLine("* Тема: Операции сравнения                                                 *");
-Console.WriteLine("* Задание #0                                                               *");
-Console.WriteLine("* Вариант #27                                                              *");
+Console.WriteLine("* Тема: Логические сравнения                                               *");
+Console.WriteLine("* Задание #1                                                               *");
+Console.WriteLine("* Вариант #3                                                               *");
 Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                                 *");
-Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,     *");
-Console.WriteLine("* вернет логическую последовательность True False True False False True    *");
+Console.WriteLine("* Написать программу из операций сравнений и логических выражений, которая *");
+Console.WriteLine("* вернет логическую последовательность True False False False False False  *");
 Console.WriteLine("*                                                                          *");
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
 Console.WriteLine("****************************************************************************");
 
-Console.WriteLine("X = " + x);
-Console.WriteLine("Y = " + y);
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine("c = " + c);
+Console.WriteLine("d = " + d);
 
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
