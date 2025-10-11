ff06d13568ec3b74129b8c6e26b52495eb299838
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-11 21:48:35 +0500

    Task3

---
+++ Tyuiu.BatTI.Sprint2.Task3.V22.Test/DataServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BatTI.Sprint2.Task3.V22.Lib;
+
+namespace Tyuiu.BatTI.Sprint2.Task3.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 27.000;
+            Assert.AreEqual(wait, res);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.750;
+            Assert.AreEqual(wait, res);
+        }
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 5.000;
+            Assert.AreEqual(wait, res);
+        }
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -100;
+            double res = ds.Calculate(x);
+            double wait = -1099.990;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
--- Tyuiu.BatTI.Sprint2.Task3.V22/Program.cs
+++ Tyuiu.BatTI.Sprint2.Task3.V22/Program.cs
@@ -1,40 +1,30 @@
-﻿using Tyuiu.BatTI.Sprint2.Task2.V29.Lib;
+﻿using Tyuiu.BatTI.Sprint2.Task3.V22.Lib;
+
+DataService ds = new DataService();
 
 Console.Title = "Спринт #2 | Выполнил: Бат Т.И. | ПИНб-25-1";
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* Спринт #2                                                                *");
-Console.WriteLine("* Тема: Оператор If                                                        *");
-Console.WriteLine("* Задание #2                                                               *");
-Console.WriteLine("* Вариант #29                                                              *");
+Console.WriteLine("* Тема: Вложенные операции                                                 *");
+Console.WriteLine("* Задание #3                                                               *");
+Console.WriteLine("* Вариант #22                                                              *");
 Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                                 *");
-Console.WriteLine("* Написать программу, которая проверяет, находится ли точка с данными      *");
-Console.WriteLine("* координатами в заштрихованной области                                    *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с     *");
+Console.WriteLine("* использованием вложенных опреаторов if-else                              *");
 Console.WriteLine("*                                                                          *");
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
 Console.WriteLine("****************************************************************************");
 
 Console.WriteLine("Введите значение переменной Х: ");
-int x = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите значение переменной Y: ");
-int y = Convert.ToInt32(Console.ReadLine());
-
-DataService ds = new DataService();
-bool res = ds.CheckDotInShadedArea(x, y);
+double x = Convert.ToDouble(Console.ReadLine());
+double res = ds.Calculate(x);
 
 Console.WriteLine("****************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
 Console.WriteLine("****************************************************************************");
 
-if (res)
-{
-    Console.WriteLine("Точка находится в заштрихованной области");
-}
-else
-{
-    Console.WriteLine("Точка не находится в заштрихованной области");
-}
+Console.WriteLine("Значение функции = " + res);
 Console.ReadKey();
