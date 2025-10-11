65c4d129171e0141969bb5d6222c9830671a67e6
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-11 18:03:59 +0500

    Task2

---
+++ Tyuiu.BatTI.Sprint2.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BatTI.Sprint2.Task2.V29.Lib
+{
+    public class DataService : ISprint2Task2V29
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if (((x < 3) && (y > 3) && (y < 6)) || ((x > 2) && (y > 2) && (x < 6) && (y < 8)) || ((x == 6) && (y > 4) && (y < 12)) || ((x > 2) && (x < 7) && (y == 11)) || ((y == 12) && (x > 3) && (x < 6)) || ((x > 6) && (x < 9) && (y > 4) && (y < 8)) || ((x > 8) && (x < 13) && (y > 2) && (y < 8)) || ((x == 13) && (y > 5) && (y < 9)) || ((x > 9) && (x < 13) && (y > 7) && (y < 12)) || ((x == 10) && (y == 12)))
+            {
+                res =  true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint2.Task2.V29/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BatTI.Sprint2.Task2.V29.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #2                                                                *");
+Console.WriteLine("* Тема: Оператор If                                                        *");
+Console.WriteLine("* Задание #2                                                               *");
+Console.WriteLine("* Вариант #29                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* Написать программу из операций сравнений и логических выражений, которая *");
+Console.WriteLine("* вернет логическую последовательность True False False False False False  *");
+Console.WriteLine("*                                                                          *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("Введите значение переменной Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+DataService ds = new DataService();
+bool res = ds.CheckDotInShadedArea(x, y);
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+if (res)
+{
+    Console.WriteLine("Точка находится в заштрихованной области");
+}
+else
+{
+    Console.WriteLine("Точка не находится в заштрихованной области");
+}
+Console.ReadKey();
