fca094b2b96ff99fde186c184449b74ee6b36842
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-12 19:05:23 +0500

    Task5

---
+++ Tyuiu.BatTI.Sprint2.Task5.V8.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BatTI.Sprint2.Task5.V8.Lib;
+
+namespace Tyuiu.BatTI.Sprint2.Task5.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+            int m = 11;
+            int n = 29;
+            var res = ds.FindDateOfPreviousDay(m, n);
+            string wait = "28.11";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint2.Task5.V8/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.BatTI.Sprint2.Task5.V8.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #2                                                                *");
+Console.WriteLine("* Тема: Оператор switch                                                    *");
+Console.WriteLine("* Задание #5                                                               *");
+Console.WriteLine("* Вариант #8                                                               *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* Написать программу, которая использует оператор switch                   *");
+Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                     *");
+Console.WriteLine("*                                                                          *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+var res = ds.FindDateOfPreviousDay(n, m);
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("Результат = " + res);
+Console.ReadKey();
