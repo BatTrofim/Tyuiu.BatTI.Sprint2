4b26ae73bdb866c7b695b4ec2fd23e64610b5cc8
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-12 20:16:27 +0500

    Task6

---
+++ Tyuiu.BatTI.Sprint2.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BatTI.Sprint2.Task6.V3.Lib;
+
+namespace Tyuiu.BatTI.Sprint2.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDayName()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            var res = ds.FindDayName(value);
+            string wait = "Понедельник";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BatTI.Sprint2.Task6.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #2                                                                *");
+Console.WriteLine("* Тема: Получение результата из switch                                     *");
+Console.WriteLine("* Задание #6                                                               *");
+Console.WriteLine("* Вариант #3                                                               *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* Написать программу, которая использует оператор switch                   *");
+Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                     *");
+Console.WriteLine("*                                                                          *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+int x = Convert.ToInt32(Console.ReadLine());
+var res = ds.FindDayName(x);
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("Результат = " + res);
+Console.ReadKey();
